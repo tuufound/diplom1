09ce9ed88dd0fd1f7f44909d55617a226bfd0251
Murat <foundeead@gmail.com>
2026-04-30 23:18:50 +0300

    dock

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -15,7 +15,7 @@ export default defineConfig({
     port: 3000,
     proxy: {
       '/api': {
-        target: 'http://localhost:8000',
+        target: 'http://backend:3000',
         changeOrigin: true,
         secure: false
       }
